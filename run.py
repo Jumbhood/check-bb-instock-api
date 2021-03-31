65e982975365965c37983277b94b273b0f7b67f0
Bo <bo.1020@hotmail.com>
2021-03-31 01:58:32 -0400

    added more scenarios for raising different messages

--- run.py
+++ run.py
@@ -1,63 +1,17 @@
 from product_list import GTX3060TI, GTX3070, GTX3080, GTX3090
-from scrape import scrape, check_stock, create_message
+from scrape import scrape, check_stock, create_message, send_request
 import requests
 import json
 
-slack_url = "INSERT_URL"
+gitslack_url = "INSERT_URL"
 slack_header = {'Content-type': 'application/json'}
 
-for g60 in GTX3060TI:
-    data = scrape(g60)
-    result = check_stock(data)
-    if result == False:
-        print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
-    else:
-        text = create_message(data)
-        myobj = {'text' : text}
-        r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
-        print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
+if __name__ == '__main__':
+    send_request(GTX3060TI, slack_url, slack_header)
+    send_request(GTX3070, slack_url, slack_header)
+    send_request(GTX3080, slack_url, slack_header)
+    send_request(GTX3090, slack_url, slack_header)
 
-for g70 in GTX3070:
-    data = scrape(g70)
-    result = check_stock(data)
-    if result == False:
-        print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
-    else:
-        text = create_message(data)
-        myobj = {'text' : text}
-        r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
-        print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
-
-for g80 in GTX3080:
-    data = scrape(g80)
-    result = check_stock(data)
-    if result == False:
-        print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
-    else:
-        text = create_message(data)
-        myobj = {'text' : text}
-        r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
-        print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
-
-for g90 in GTX3090:
-    data = scrape(g90)
-    result = check_stock(data)
-    if result == False:
-        print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
-    else:
-        text = create_message(data)
-        myobj = {'text' : text}
-        r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
-        print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
-
-# test = ['10416248']
-# for t in test:
-#     data = scrape(t)
-#     result = check_stock(data)
-#     if result == False:
-#         print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
-#     else:
-#         text = create_message(data)
-#         myobj = {'text' : text}
-#         r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
-#         print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
+    # Test list, put any product id in the list for testing purposes
+    #testlist = ['12383793']
+    #send_request(testlist, slack_url, slack_header)
--- scrape.py
+++ scrape.py
@@ -8,13 +8,18 @@ def scrape(product_id):
     full_url = api_url + product_id
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
     }
-    response = requests.get(full_url, headers=headers).json()
-    result = {
-        'name' : response['name'],
-        'isPurchasable' : response['isPurchasable'],
-        'productUrl' : response['productUrl']
-    }
-    return result
+    response = requests.get(full_url, headers=headers)
+    if response.status_code == 200:
+        response = response.json()
+        result = {
+            'name' : response['name'],
+            'isPurchasable' : response['isPurchasable'],
+            'productUrl' : response['productUrl'],
+            'salePrice' : response['salePrice']
+        }
+        return result
+    else:
+        return False
 
 def check_stock(data):
     is_purchasable = data['isPurchasable']
@@ -27,5 +32,23 @@ def create_message(data):
     name =  data['name']
     is_on_sale = data['isPurchasable']
     product_url = data['productUrl']
-    text = 'Hey <!everyone>! Seems like `{}` is in stock now, check :alert:<{}|*Purchase Link*>:alert:'.format(name, product_url)
+    sale_price = data['salePrice']
+    text = 'Hey <!everyone>! Seems like `{}` is in stock now for `{} CAD`, check :alert:<{}|*Purchase Link*>:alert:'.format(name, sale_price, product_url)
     return text
+
+def send_request(card_list, slack_url, slack_header):
+    for card in card_list:
+        data = scrape(card)
+        if data == False:
+            print("[ ERROR ] - Can't connect to the server!")
+        else:
+            result = check_stock(data)
+            if result == False:
+                print("[ INFO ] - {} - {}".format(data['name'], "Not in Stock"))
+            elif result == True:
+                text = create_message(data)
+                myobj = {'text' : text}
+                r = requests.post(slack_url, data = json.dumps(myobj), headers = slack_header)
+                print("[ ALERT ] - {} - {}".format(data['name'], "In Stock"))
+            else:
+                print("[ ALERT ] - {} - {}".format(data['name'], "Unknown Status"))
